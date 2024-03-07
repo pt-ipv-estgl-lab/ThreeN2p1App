5ca0cbbd188a58fd42fd4fb6508f9834bea4bbff
Carlos J. A. Costa <ccosta@estgl.ipv.pt>
2024-03-07 12:46:46 +0000

    Initial test

---
+++ ThreeN2.java
@@ -0,0 +1,15 @@
+/**
+ * ThreeN2
+ * A program that computes and displays several 3N+1 sequences. Starting
+ * values for the sequences are input by the user. Terms in the sequence
+ * are printed in columns, with five terms on each line of output.
+ * After a sequence has been displayed, the number of terms in that
+ * sequence is reported to the user.
+ */
+public class ThreeN2 {
+    public static void main(String[] args) {
+        System.out.println("This program will print out 3N+1 sequences");
+        System.out.println("for starting values that you specify.");
+        System.out.println();
+    }
+}
